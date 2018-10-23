9d0bdda568abdfe03a416ffbff6414d432f34e80
nnhoangit4 <44374517+nnhoangit4@users.noreply.github.com>
2018-10-23 16:28:42 +0700

    Add files via upload

---
+++ mang.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void nhapmang(int A[],int n ){
+	for (int i=0 ;i<n;i++){
+		prinf(" nhap gia tri A[%d] = ",i);
+		scanf("%d",&A[i]);
+	}
+}
+void xuatmang(intA[],int n ){
+	printf(" mang moi nhap ");
+	for (int i=0;i<n;i++){
+		printf("%d ",A[i]);
+		
+	}
+}
+void giaithua(int s,int i){
+	
+}
+
+int main (){
+	int A[];
+	int n 
+	nhapmang(A,n);
+	xuatmang(A,n);
+}
